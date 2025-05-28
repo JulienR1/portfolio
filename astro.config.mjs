f3b8f3e3fc0c3c424c9e6064b4028d5c2686074f
julienrousseau <julrousseau20@gmail.com>
2025-05-28 12:14:14 -0400

    fixed vite build warning and added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,40 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { resolve } from "path";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://julienrousseau.ca",
+
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
     fallback: { fr: "en" },
     routing: { fallbackType: "rewrite" },
   },
+
+  build: {
+    assets: "res",
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        "@fonts": resolve("./src/fonts"),
+      },
+    },
+  },
+
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          fr: "fr-CA",
+        },
+      },
+    }),
+  ],
 });
